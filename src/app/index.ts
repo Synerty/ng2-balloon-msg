ff1e478a80aaf236a9cfd2048dbdb4d1df3ef1ba
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-22 21:15:06 +1000

    Completed package.json

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,2 @@
-export {Ng2BalloonMsgModule} from "../modules/ng2-balloon-msg.module";
-export {Ng2BalloonMsgService} from "../services/service/ng2-balloon-msg.service";
-
 export * from './app.component';
 export * from './app.module';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export {Ng2BalloonMsgModule} from "./modules/ng2-balloon-msg.module";
+export {Ng2BalloonMsgService} from "./services/service/ng2-balloon-msg.service";
