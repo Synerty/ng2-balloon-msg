9c32a577067bf334f6f6111e3c10ecaa83836453
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-09-10 17:50:23 +1000

    Attempts at fixing nativescript buttons on iOS bug

--- ng2-balloon-msg/nativescript/item/ng2-balloon-msg-ns-item.component.ts
+++ ng2-balloon-msg/nativescript/item/ng2-balloon-msg-ns-item.component.ts
@@ -6,7 +6,6 @@ import {Ng2BalloonMsgCommonItemComponent} from "../../common/ng2-balloon-msg-com
 @Component({
     selector: 'ng2-balloon-msg-item-ns',
     templateUrl: './ng2-balloon-msg-ns-item.component.html',
-    styleUrls: ['./ng2-balloon-msg-ns-item.component.css'],
     moduleId: module.id
 })
 export class Ng2BalloonMsgNsItemComponent extends Ng2BalloonMsgCommonItemComponent {
