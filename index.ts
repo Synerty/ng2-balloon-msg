c17146cf551eba964dac14662500232d3dd54dd1
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-13 21:13:53 +1000

    added item/queue classes to export

--- index.ts
+++ index.ts
@@ -7,3 +7,5 @@ export {
 } from "./ng2-balloon-msg/services/ng2-balloon-msg.service";
 
 
+export { Ng2BalloonMsgCommonItemComponent } from './ng2-balloon-msg/common/ng2-balloon-msg-common-item.component';
+export { Ng2BalloonMsgCommonQueueComponent } from './ng2-balloon-msg/common/ng2-balloon-msg-common-queue.component';
